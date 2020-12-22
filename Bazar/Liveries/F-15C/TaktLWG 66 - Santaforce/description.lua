e78f90d6dd84dea8959db68a4069a363460a7d39
Basti <giveafuck@gmail.com>
2020-12-22 21:44:11 +0100

    Santaforce, Baby!

---
+++ Bazar/Liveries/F-15C/TaktLWG 66 - Santaforce/description.lua
@@ -0,0 +1,19 @@
+livery = {
+	{"f15nose", 0 ,"f15_nose_santa",false};
+	{"f15nose", 3 ,"f15_decol_santa",false};
+	{"f15bottom", 0 ,"f15_bottom_santa",false};
+	{"f15bottom", 3 ,"f15_decol_santa",false};
+	{"f15centr", 0 ,"f15_centr_santa",false};
+	{"f15wingl", 0 ,"f15_wing_l_santa",false};
+	{"f15wingl", 3 ,"f15_decol_santa",false};
+	{"f15wingr", 0 ,"f15_wing_r_santa",false};
+	{"f15stab", 0 ,"f15_stab_v_santa",false};
+	{"f15stab", 3 ,"f15_decol_santa",false};
+	{"f15numbers", 0 ,"f15_numbers_santa",false};
+	{"pilot_F15_patch", 0 ,"pilot_F15_patch_65A_sqdn",true};
+	{"pilot_F15_00_body", 0 ,"pilot_f15_00_a",true};
+	{"f15PTB", 0 ,"f15_PTB_santa",false};
+	
+}
+
+name = "TaktLWG 66 - Santaforce"
